bc03551fb88b48b99530afd4aa9a0e7183b677fc
Pipa <damianpeaf@gmail.com>
2020-09-21 19:26:04 -0600

    archivos iniciales

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+</head>
+<body>
+        <div class="bg-dark ">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-dark">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                      <div class=" navg navbar-nav w-100 justify-content-center " >
+                        <a class="nav-item nav-link active" href="./index.php">Inicio</a>
+                        <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            ¿Quienes somos?
+                          </a>
+                          <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Mision</a>
+                            <a class="dropdown-item" href="#">Vision</a>
+                          </div>
+                        </li>
+                        <a class="nav-item nav-link" href="#">Paquetes de los servicios</a>
+                        <a class="nav-item nav-link" href="./Horarios.php">Horario</a>
+                        <a class="nav-item nav-link" href="#">contacto</a>
+                      </div>
+                    </div>
+                  </nav>
+            </div>
+        </div>
+        
+</body>
+</html>
